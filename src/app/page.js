ecbad7b7b5bf44c7922e6eb05997192483489e63
Zubair Cheema <cheema4310@gmail.com>
2024-05-18 03:51:24 +0500

    fix some issue

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <main>
       <Hero />
-      <div className="fixed w-full h-24 bottom-0 left-0 overflow-hidden bg-dark">
+      {/* <div className="fixed w-full h-24 bottom-0 left-0 overflow-hidden bg-dark">
         {process.env.NODE_ENV === 'production' && <AdComponent />}
-      </div>
+      </div> */}
     </main>
   );
 }
